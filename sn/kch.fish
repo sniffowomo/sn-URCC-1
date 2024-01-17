5bac1c2ef780ad33bf9c3e910ebd5df95d8ca757
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2024-01-17 13:10:05 +0000

    .

--- sn/kch.fish
+++ sn/kch.fish
@@ -1,10 +1,10 @@
 #!/bin/fish
-/workspaces/UCPD/wb/pnp.fish
-/workspaces/UCPD/wb/fixrs.fish
-/workspaces/UCPD/wb/st.sh
-/workspaces/UCPD/wb/aryt.fish
-/workspaces/UCPD/wb/tgpt.fish
-/workspaces/UCPD/wb/bun.fish
-/workspaces/UCPD/wb/tmuxx.fish
+/workspaces/sn-URCC-1/sn/pnp.fish
+/workspaces/sn-URCC-1/sn/fixrs.fish
+/workspaces/sn-URCC-1/sn/st.sh
+/workspaces/sn-URCC-1/sn/aryt.fish
+/workspaces/sn-URCC-1/sn/tgpt.fish
+/workspaces/sn-URCC-1/sn/bun.fish
+/workspaces/sn-URCC-1/sn/tmuxx.fish
 
 
